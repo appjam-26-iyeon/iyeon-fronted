2f9e3e924b02d376ae1e2ee6d7f955596d9e44ce
jgh진건희 <s23054@gsm.hs.kr>
2024-06-22 21:36:06 +0900

    setting: router 설정

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+function Router() {
+    return(
+        <BrowserRouter>
+            <Route></Route>
+        </BrowserRouter>
+    )
+}
+
+export default Router
